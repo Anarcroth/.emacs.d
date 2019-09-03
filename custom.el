e5e4efb67ab02f24ff5d1d032c1a583dc2dfa9ae
Martin Nestorov <mnestorov@protonmail.com>
2019-09-03 09:53:14 +0300

    Add custom.el

---
+++ custom.el
@@ -0,0 +1,29 @@
+;; Set coding styles and indents
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(c-default-style
+   (quote
+    ((other . "stroustrup")
+     (java-mode . "java")
+     (awk-mode . "awk")
+     (other . "gnu"))))
+ '(elpy-modules
+   (quote
+    (elpy-module-company elpy-module-eldoc elpy-module-pyvenv elpy-module-highlight-indentation elpy-module-yasnippet elpy-module-django elpy-module-sane-defaults)))
+ '(js2-basic-offset 2)
+ '(package-selected-packages
+   (quote
+    (ac-js2 org-pomodoro spaceline diminish smooth-scrolling xref-js2 js2-refactor js2-mode yaml-mode flycheck-irony company-irony irony historian latex-math-preview flycheck-rtags rtags company-rtags htmlize rainbow-delimiters ac-slime slime highlight-thing diff-hl gitconfig-mode gitignore-mode pip-requirements anaconda-mode whitespace-cleanup-mode highlight-escape-sequences flycheck-pos-tip flycheck multiple-cursors pdf-tools markdown-mode wrap-region winum vimish-fold ox-reveal nyan-mode neotree magit flyspell-correct elpy auctex-latexmk atom-one-dark-theme)))
+ '(sh-basic-offset 2)
+ '(sh-indentation 2)
+ '(smie-indent-basic 2))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(hi-yellow ((t (:background "#3E4451"))))
+ '(spaceline-highlight-face ((t (:foreground "#282C34" :background "#528BFF")))))
