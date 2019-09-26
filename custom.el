ebc5ac9538d8f5a24edb32851659671d3fa27b29
Martin Nestorov <mnestorov@protonmail.com>
2019-09-26 17:49:49 +0300

    Add spaceline-all-the-icons custom setup

--- custom.el
+++ custom.el
@@ -19,7 +19,17 @@
     (fancy-battery paradox spaceline-all-the-icons all-the-icons ac-js2 org-pomodoro diminish smooth-scrolling xref-js2 js2-refactor js2-mode yaml-mode flycheck-irony company-irony irony historian latex-math-preview flycheck-rtags rtags company-rtags htmlize rainbow-delimiters ac-slime slime highlight-thing diff-hl gitconfig-mode gitignore-mode pip-requirements anaconda-mode whitespace-cleanup-mode highlight-escape-sequences flycheck-pos-tip flycheck multiple-cursors pdf-tools markdown-mode wrap-region winum vimish-fold ox-reveal nyan-mode neotree magit flyspell-correct elpy auctex-latexmk atom-one-dark-theme)))
  '(sh-basic-offset 2)
  '(sh-indentation 2)
- '(smie-indent-basic 2))
+ '(smie-indent-basic 2)
+ '(spaceline-all-the-icons-flycheck-alternate nil)
+ '(spaceline-all-the-icons-hide-long-buffer-path t)
+ '(spaceline-all-the-icons-highlight-file-name t)
+ '(spaceline-all-the-icons-icon-set-eyebrowse-slot (quote square))
+ '(spaceline-all-the-icons-icon-set-git-ahead (quote commit))
+ '(spaceline-all-the-icons-icon-set-window-numbering (quote square))
+ '(spaceline-all-the-icons-primary-separator "")
+ '(spaceline-all-the-icons-separator-type (quote arrow))
+ '(spaceline-all-the-icons-slim-render nil)
+ '(spaceline-all-the-icons-window-number-always-visible t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
