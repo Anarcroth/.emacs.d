fa8eebc420f415fb918f2c55aa1641648fee6547
Martin Nestorov <mnestorov@protonmail.com>
2019-09-27 18:06:39 +0300

    Add new colors to the org faces

--- custom.el
+++ custom.el
@@ -13,10 +13,14 @@
  '(elpy-modules
    (quote
     (elpy-module-company elpy-module-eldoc elpy-module-pyvenv elpy-module-highlight-indentation elpy-module-yasnippet elpy-module-django elpy-module-sane-defaults)))
- '(js2-basic-offset 2)
+ '(js2-basic-offset 4)
+ '(org-todo-keywords
+   (quote
+    ((sequence "TODO(t)" "IN-PROGRESS(p)" "TESTING(e)" "WAITING(w)" "|" "DONE(d)" "CANCELED(c)" "DIDN'T(n)")
+     (sequence "IDEA(i)" "RE-THINK(r)" "LATER(l)" "APPOINTMENT(a)" "|"))))
  '(package-selected-packages
    (quote
-    (fancy-battery paradox spaceline-all-the-icons all-the-icons ac-js2 org-pomodoro diminish smooth-scrolling xref-js2 js2-refactor js2-mode yaml-mode flycheck-irony company-irony irony historian latex-math-preview flycheck-rtags rtags company-rtags htmlize rainbow-delimiters ac-slime slime highlight-thing diff-hl gitconfig-mode gitignore-mode pip-requirements anaconda-mode whitespace-cleanup-mode highlight-escape-sequences flycheck-pos-tip flycheck multiple-cursors pdf-tools markdown-mode wrap-region winum vimish-fold ox-reveal nyan-mode neotree magit flyspell-correct elpy auctex-latexmk atom-one-dark-theme)))
+    (org-bullets org-fancy-priorities fancy-battery paradox spaceline-all-the-icons all-the-icons ac-js2 org-pomodoro diminish smooth-scrolling xref-js2 js2-refactor js2-mode yaml-mode flycheck-irony company-irony irony historian latex-math-preview flycheck-rtags rtags company-rtags htmlize rainbow-delimiters ac-slime slime highlight-thing diff-hl gitconfig-mode gitignore-mode pip-requirements anaconda-mode whitespace-cleanup-mode highlight-escape-sequences flycheck-pos-tip flycheck multiple-cursors pdf-tools markdown-mode wrap-region winum vimish-fold ox-reveal nyan-mode neotree magit flyspell-correct elpy auctex-latexmk atom-one-dark-theme)))
  '(sh-basic-offset 2)
  '(sh-indentation 2)
  '(smie-indent-basic 2)
@@ -36,9 +40,18 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(hi-yellow ((t (:background "#3E4451"))))
+ '(org-agenda-done ((t (:foreground "#98C379"))))
+ '(org-done ((t (:foreground "#98C379" :weight bold))))
+ '(org-headline-done ((t (:foreground "#98C379"))))
+ '(org-level-1 ((t (:background "#121417" :foreground "#528BFF" :weight bold))))
+ '(org-level-2 ((t (:background "#121417" :foreground "#E06C75" :weight bold))))
+ '(org-level-3 ((t (:foreground "#61AFEF" :weight bold))))
+ '(org-level-4 ((t (:foreground "#E5C07B" :underline (:color foreground-color :style wave)))))
+ '(org-todo ((t (:foreground "#E06C75" :weight bold))))
+ '(org-upcoming-deadline ((t (:foreground "#BE5046"))))
  '(spaceline-all-the-icons-sunrise-face ((t (:foreground "#f6c175"))))
  '(spaceline-all-the-icons-sunset-face ((t (:foreground "#fe7714"))))
  '(spaceline-highlight-face ((t (:foreground "#282C34" :background "#528BFF"))))
  '(spaceline-python-venv ((t (:foreground "#528BFF"))))
- '(whitespace-tab ((t (:foreground "#4B5363"))))
- '(whitespace-space ((t (:foreground "#4B5363")))))
+ '(whitespace-space ((t (:foreground "#4B5363"))))
+ '(whitespace-tab ((t (:foreground "#4B5363")))))
