ac6f8cb4a9c4c0a2c18b4c4a91e86ed02b74add2
Martin Nestorov <mnestorov@protonmail.com>
2019-09-30 11:59:15 +0300

    Add mc for region selection

--- init.el
+++ init.el
@@ -215,7 +215,8 @@
 (global-set-key (kbd "C-c r e") 'eval-region)
 
 ;; Multiple cursors
-;; (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(require 'multiple-cursors)
+(global-set-key (kbd "C-c C-S-c") 'mc/edit-lines)
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
