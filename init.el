798f9f8dc61b0d26a37322bb6163f3ac113f8c24
Martin Nestorov <mnestorov@protonmail.com>
2020-04-20 22:27:29 +0300

    Add goto paranthesis matching function

--- init.el
+++ init.el
@@ -185,6 +185,25 @@
 (setq company-tooltip-limit 20)
 (setq company-selection-wrap-around t)
 
+(defun goto-match-paren (arg)
+  "Go to the matching parenthesis if on parenthesis.
+Else go to the opening parenthesis one level up."
+  (interactive "p")
+  (cond ((looking-at "\\s\(") (forward-list 1))
+	(t
+	 (backward-char 1)
+	 (cond ((looking-at "\\s\)")
+		(forward-char 1) (backward-list 1))
+	       (t
+		(while (not (looking-at "\\s("))
+		  (backward-char 1)
+		  (cond ((looking-at "\\s\)")
+			 (message "->> )")
+			 (forward-char 1)
+			 (backward-list 1)
+			 (backward-char 1)))))))))
+(global-set-key (kbd "C-M-g") 'goto-match-paren)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; +-----------------------+ ;;
 ;; |   Dev environment     | ;;
