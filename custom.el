50d382ad405fda6bb2ab4b80e383b8ca190f8f3f
Martin Nestorov <mnestorov@protonmail.com>
2020-09-08 15:00:01 +0300

    Format custom.el
    Use quotes and add docstrings

--- custom.el
+++ custom.el
@@ -1,4 +1,11 @@
+;;; package --- Summary
+;; Many different custom variables.
+
+;;; Commentary:
+;; Doom setup, icons, dev styles, packages, elpy modules, many many faces.
 ;; Set coding styles and indents
+
+;;; Code:
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -7,40 +14,36 @@
  '(all-the-icons-default-adjust 0.0)
  '(all-the-icons-scale-factor 1)
  '(c-default-style
-   (quote
-    ((other . "stroustrup")
+   '((other . "stroustrup")
      (java-mode . "java")
      (awk-mode . "awk")
-     (other . "gnu"))))
+     (other . "gnu")))
  '(doom-modeline-bar-width 10)
  '(doom-modeline-buffer-encoding t)
- '(doom-modeline-buffer-file-name-style (quote buffer-name))
+ '(doom-modeline-buffer-file-name-style 'buffer-name)
  '(doom-modeline-env-enable-python nil)
  '(doom-modeline-env-version t)
  '(doom-modeline-height 25)
  '(doom-modeline-persp-name t)
  '(elpy-modules
-   (quote
-    (elpy-module-company elpy-module-eldoc elpy-module-pyvenv elpy-module-highlight-indentation elpy-module-yasnippet elpy-module-django elpy-module-sane-defaults)))
+   '(elpy-module-company elpy-module-eldoc elpy-module-pyvenv elpy-module-highlight-indentation elpy-module-yasnippet elpy-module-django elpy-module-sane-defaults))
  '(js2-basic-offset 4)
  '(org-todo-keywords
-   (quote
-    ((sequence "TODO(t)" "IN-PROGRESS(p)" "TESTING(e)" "WAITING(w)" "|" "DONE(d)" "CANCELED(c)" "DIDN'T(n)")
-     (sequence "IDEA(i)" "RE-THINK(r)" "LATER(l)" "APPOINTMENT(a)" "|"))))
+   '((sequence "TODO(t)" "IN-PROGRESS(p)" "TESTING(e)" "WAITING(w)" "|" "DONE(d)" "CANCELED(c)" "DIDN'T(n)")
+     (sequence "IDEA(i)" "RE-THINK(r)" "LATER(l)" "APPOINTMENT(a)" "|")))
  '(package-selected-packages
-   (quote
-    (lsp-mode hl-todo projectile auto-package-update which-key with-editor tablist swiper scratch pyvenv lispy pdf-tools iedit highlight-indentation find-file-in-project counsel ace-window transient elpy magit ivy flycheck-clojure flycheck-inline-mode flycheck-mode racer toml-mode cargo flycheck-rust rust-mode vlf clj-refactor-mode clj-refactor cider clojure-mode doom-modeline undo-tree org-bullets org-fancy-priorities fancy-battery paradox spaceline-all-the-icons all-the-icons ac-js2 org-pomodoro diminish smooth-scrolling xref-js2 js2-refactor js2-mode yaml-mode flycheck-irony company-irony latex-math-preview flycheck-rtags company-rtags htmlize rainbow-delimiters ac-slime highlight-thing diff-hl gitconfig-mode gitignore-mode pip-requirements whitespace-cleanup-mode highlight-escape-sequences flycheck-pos-tip multiple-cursors markdown-mode wrap-region winum vimish-fold ox-reveal neotree flyspell-correct auctex-latexmk atom-one-dark-theme)))
+   '(lsp-ui lsp-mode hl-todo projectile auto-package-update which-key with-editor tablist swiper scratch pyvenv lispy pdf-tools iedit highlight-indentation find-file-in-project counsel ace-window transient elpy magit ivy flycheck-clojure flycheck-inline-mode flycheck-mode racer toml-mode cargo flycheck-rust rust-mode vlf clj-refactor-mode clj-refactor cider clojure-mode doom-modeline undo-tree org-bullets org-fancy-priorities fancy-battery paradox spaceline-all-the-icons all-the-icons ac-js2 org-pomodoro diminish smooth-scrolling xref-js2 js2-refactor js2-mode yaml-mode flycheck-irony company-irony latex-math-preview flycheck-rtags company-rtags htmlize rainbow-delimiters ac-slime highlight-thing diff-hl gitconfig-mode gitignore-mode pip-requirements whitespace-cleanup-mode highlight-escape-sequences flycheck-pos-tip multiple-cursors markdown-mode wrap-region winum vimish-fold ox-reveal neotree flyspell-correct auctex-latexmk atom-one-dark-theme))
  '(sh-basic-offset 4)
  '(sh-indentation 2)
  '(smie-indent-basic 2)
  '(spaceline-all-the-icons-flycheck-alternate nil)
  '(spaceline-all-the-icons-hide-long-buffer-path t)
  '(spaceline-all-the-icons-highlight-file-name t)
- '(spaceline-all-the-icons-icon-set-eyebrowse-slot (quote square))
- '(spaceline-all-the-icons-icon-set-git-ahead (quote commit))
- '(spaceline-all-the-icons-icon-set-window-numbering (quote square))
+ '(spaceline-all-the-icons-icon-set-eyebrowse-slot 'square)
+ '(spaceline-all-the-icons-icon-set-git-ahead 'commit)
+ '(spaceline-all-the-icons-icon-set-window-numbering 'square)
  '(spaceline-all-the-icons-primary-separator "")
- '(spaceline-all-the-icons-separator-type (quote arrow))
+ '(spaceline-all-the-icons-separator-type 'arrow)
  '(spaceline-all-the-icons-slim-render nil)
  '(spaceline-all-the-icons-window-number-always-visible t))
 (custom-set-faces
@@ -86,3 +89,4 @@
  '(spaceline-python-venv ((t (:foreground "#528BFF"))))
  '(whitespace-space ((t (:foreground "#4B5363"))))
  '(whitespace-tab ((t (:foreground "#4B5363")))))
+;;; custom.el ends here
