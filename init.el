93b0ac53413a2e4e289a06ddf2032a743f7e394f
Martin Nestorov <mnestorov@protonmail.com>
2023-05-10 16:26:20 +0300

    Add emacs whitespace mode config

--- init.el
+++ init.el
@@ -977,14 +977,44 @@ DIR is handled as by `windmove-other-window-loc'."
 (global-visual-line-mode t)
 
 ;; Setup whitespaces to be depicted with dots
-;; Then start `whitespace-mode` in order to see the spaces/tabs
-(setq whitespace-style '(face spaces space-mark face tab tab-mark trailing))
-(setq whitespace-display-mappings
-      '(
-	;; this is a mapping for the '—' character
-	(tab-mark ?\t [?\u2014 ?\t] [92 ?\t])
-	;; this is a mapping for the '∙' character
-	(space-mark ?\ [?\u2219] [46])))
+;; Define the whitespace style.
+(setq-default whitespace-style
+              '(face spaces empty tabs newline trailing space-mark tab-mark newline-mark))
+;; Whitespace color corrections.
+(require 'color)
+(let* ((ws-lighten 30) ;; Amount in percentage to lighten up black.
+       (ws-color (color-lighten-name "#000000" ws-lighten)))
+  (custom-set-faces
+   `(whitespace-newline                ((t (:foreground ,ws-color))))
+   `(whitespace-missing-newline-at-eof ((t (:foreground ,ws-color))))
+   `(whitespace-space                  ((t (:foreground ,ws-color))))
+   `(whitespace-space-after-tab        ((t (:foreground ,ws-color))))
+   `(whitespace-space-before-tab       ((t (:foreground ,ws-color))))
+   `(whitespace-tab                    ((t (:foreground ,ws-color))))
+   `(whitespace-trailing               ((t (:foreground ,ws-color))))))
+;; Make these characters represent whitespace.
+(setq-default whitespace-display-mappings
+	      '(
+		;; space -> · else .
+		(space-mark 32 [183] [46])
+		;; new line -> ¬ else $
+		(newline-mark ?\n [172 ?\n] [36 ?\n])
+		;; carriage return (Windows) -> ¶ else #
+		(newline-mark ?\r [182] [35])
+		;; tabs -> » else >
+		(tab-mark ?\t [187 ?\t] [62 ?\t])))
+;; Don't enable whitespace for.
+(setq-default whitespace-global-modes
+              '(not shell-mode
+		    help-mode
+		    magit-mode
+		    magit-diff-mode
+		    ibuffer-mode
+		    dired-mode
+		    occur-mode
+		    lsp-ui-mode
+		    lsp-mode))
+(global-whitespace-mode 1)
 
 ;; Unique names of buffers for files with identical names
 (paradox-require 'uniquify)
